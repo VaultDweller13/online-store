0469841455a4e98c354dd19b50b92bc2cd83b9c8
Polina <sunnyfur9@gmail.com>
2022-12-17 11:37:23 +0500

    feat: add img and price to card

--- src/components/view/product/productsView.ts
+++ src/components/view/product/productsView.ts
@@ -1,11 +1,16 @@
 import { createElemDOM } from './../../../assets/ts/createElementDom';
 import './../../../assets/styles/components/products.scss';
-import './products.html';
 
 export class ProductsView {
   private static drawCard(data: ProductData): HTMLElement {
     const card = createElemDOM('div', 'card');
     card.appendChild(createElemDOM('h3', '', data.title));
+    const img = createElemDOM('img', 'card__img') as HTMLImageElement;
+    card.appendChild(img);
+    img.alt = data.title;
+    img.src = data.images[0];
+    card.appendChild(createElemDOM('p', '', data.price.toString()));
+
     const button = createElemDOM('button', 'button', 'Add to cart');
     card.appendChild(button);
     // TODO add event listener Cart.addTocart()
