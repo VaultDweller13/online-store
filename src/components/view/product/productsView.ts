09db416b75c8dbdbefd02b77aa775330d0f11f0f
Polina <sunnyfur9@gmail.com>
2023-01-09 22:25:19 +0500

    deat: add details to card

--- src/components/view/product/productsView.ts
+++ src/components/view/product/productsView.ts
@@ -1,3 +1,6 @@
+import { OrderView } from './../order/orderView';
+import { DialogView } from './../dialogView';
+import { CartView } from './../cart/cartView';
 import { Cart } from './../../cart/cart';
 import { createElemDOM } from '../../../utils/utils';
 import './../../../assets/styles/components/products.scss';
@@ -59,10 +62,63 @@ export class ProductsView {
     container.append(fragment);
   }
 
-  public static drawProduct(data: ProductData, cart: Cart): void {
+  public static drawProduct(product: ProductData, cart: Cart): void {
     const fragment = document.createDocumentFragment();
     const container = document.querySelector('.main');
     if (!container) throw new Error("Can't find element with class 'main'");
+    const card = createElemDOM('div', 'card_single');
+    const title = createElemDOM('h3', 'card__title', product.title);
+    // const img = createElemDOM('img', 'card__img');
+    // if (img instanceof HTMLImageElement) {
+    //   img.alt = product.title;
+    //   img.src = product.images[0];
+    // }
+    const price = createElemDOM('p', 'card__price', `Price: ${product.price}`);
+    const stock = createElemDOM('p', 'card__stock', `Stock: ${product.stock}`);
+    const images = createElemDOM('div', 'card__images');
+    let i = 0;
+    const countInCart = cart.getProductCount(product);
+    product.images.forEach((currImg) => {
+      const currImgHTML = createElemDOM('img', 'images__img');
+      if (currImgHTML instanceof HTMLImageElement) {
+        console.log(currImg);
+        currImgHTML.src = currImg;
+        currImgHTML.alt = `img${i++}`;
+        images.append(currImgHTML);
+      }
+    });
+    const button = createElemDOM(
+      'button',
+      `button button_add ${countInCart ? 'hide' : ''}`,
+      'Add to cart'
+    );
+    const inCartWrapper = createElemDOM(
+      'div',
+      `card__cart ${!countInCart ? 'hide' : ''}`
+    );
+
+    const count = createElemDOM('p', 'card__count', countInCart.toString());
+    const buttonInc = createElemDOM('button', 'button button_inc', '+');
+    const buttonDec = createElemDOM('button', 'button button_dec', '-');
+    const buttonBuy = createElemDOM('button', 'button button__buy', 'Buy now');
+
+    inCartWrapper.append(buttonDec, count, buttonInc);
+    card.append(
+      title,
+
+      price,
+      stock,
+      images,
+      button,
+      inCartWrapper,
+      buttonBuy
+    );
+    fragment.append(card);
+    buttonBuy.addEventListener('click', () => {
+      if (!countInCart) cart.addProduct(product);
+      CartView.draw(cart.cartProducts);
+      DialogView.draw(OrderView.draw(cart));
+    });
 
     container.textContent = '';
     container.append(fragment);
