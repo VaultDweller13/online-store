21f1afde523f517d8bbcce99e5bb65fb15e99ab3
Artyom <45403328+VaultDweller13@users.noreply.github.com>
2023-01-09 08:49:34 +0400

    Feat/filter page query parameters (#12)
    * feat: add top panel styles
    
    * feat: add Router class, query-parameters now added to current url
    
    * feat: add query parameters check on filter page loading
    
    * fix: fix sliders query parameters parsing
    
    * feat: add reset and copy buttons without logic
    
    * feat: add reset button logic
    
    * feat: add copy button logic
    
    * fix: fix query params reset

---
+++ src/components/controller/router.ts
@@ -0,0 +1,46 @@
+export default class Router {
+  url: URL;
+  params: URLSearchParams;
+
+  constructor() {
+    this.url = new URL(window.location.href);
+    this.params = new URLSearchParams(this.url.search);
+  }
+
+  setQuery(name: string, value: string): void {
+    this.params.set(name, value);
+    window.history.replaceState(null, '', `?${this.params.toString()}`);
+    // this.url = new URL(window.location.href);
+  }
+
+  deleteQuery(name: string) {
+    this.params.delete(name);
+    const url = this.params.toString()
+      ? `?${this.params.toString()}`
+      : this.url.origin;
+    window.history.replaceState(null, '', url);
+  }
+
+  getQuery(name: string): string | null {
+    const value = this.params.get(name);
+    return value;
+  }
+
+  getQueryString(): string | null {
+    const value = this.params.toString();
+    return value;
+  }
+
+  resetURL() {
+    window.history.replaceState(null, '', this.url.origin);
+    this.url.search = '';
+    this.params = new URLSearchParams(this.url.search);
+  }
+
+  copyURL() {
+    navigator.clipboard.writeText(window.location.href).then(
+      () => console.log('Copied!'),
+      () => console.log('Failed to copy')
+    );
+  }
+}
