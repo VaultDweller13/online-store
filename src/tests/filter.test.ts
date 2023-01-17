ff1f5d832eef6510673fb072fc9a8dce1c2d5036
VaultDweller13 <grognak13@gmail.com>
2023-01-18 00:52:55 +0400

    feat: add Filter class unit tests

---
+++ src/tests/filter.test.ts
@@ -0,0 +1,52 @@
+import Filter from '../components/filter/filter';
+const data = [
+  {
+    id: 1,
+    title: 'iPhone 9',
+    description: 'An apple mobile which is nothing like apple',
+    price: 549,
+    discountPercentage: 12.96,
+    rating: 4.69,
+    stock: 94,
+    brand: 'Apple',
+    category: 'smartphones',
+    thumbnail: 'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
+    images: [
+      'https://i.dummyjson.com/data/products/1/1.jpg',
+      'https://i.dummyjson.com/data/products/1/2.jpg',
+      'https://i.dummyjson.com/data/products/1/3.jpg',
+      'https://i.dummyjson.com/data/products/1/4.jpg',
+      'https://i.dummyjson.com/data/products/1/thumbnail.jpg',
+    ],
+  },
+  {
+    id: 2,
+    title: 'iPhone X',
+    description:
+      'SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...',
+    price: 899,
+    discountPercentage: 17.94,
+    rating: 4.44,
+    stock: 34,
+    brand: 'Apple',
+    category: 'smartphones',
+    thumbnail: 'https://i.dummyjson.com/data/products/2/thumbnail.jpg',
+    images: [
+      'https://i.dummyjson.com/data/products/2/1.jpg',
+      'https://i.dummyjson.com/data/products/2/2.jpg',
+      'https://i.dummyjson.com/data/products/2/3.jpg',
+      'https://i.dummyjson.com/data/products/2/thumbnail.jpg',
+    ],
+  },
+];
+
+const filter = new Filter(data);
+
+describe('getRange()', () => {
+  it('should return array of length 2', () => {
+    const priceRange = filter['getRange'](data, 'price');
+
+    expect(Array.isArray(priceRange)).toBeTruthy();
+    expect(priceRange).toHaveLength(2);
+  });
+});
